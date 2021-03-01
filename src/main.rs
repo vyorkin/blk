619f18fd60ac7cdf67c6750b1b1d6fa9e67f6fc1
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2021-03-01 16:39:08 +0300

    Fix difficulty

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use blk::{block, Block};
 
 fn main() {
+    let difficulty = 0x00FFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
     let mut block = Block::new(
         0,
         0,
         block::Hash::from(vec![0; 32]),
         0,
         "Genesis block",
-        0x000000FFFFFFFFFFFFFFFFFFFFFFFFFF,
+        difficulty,
     );
     block.hash = block.gen_hash();
     println!("{:?}", &block);
